52fd621d32a85852629a7325a21d59e0b26c75d9
DwainAnderson <153378199+DwainAnderson@users.noreply.github.com>
2024-05-01 00:55:32 +0000

    prog

--- backend/src/recommendations.py
+++ backend/src/recommendations.py
@@ -1,16 +1,17 @@
-import numpy
 from app import *
 from init import app, db
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
 from mpl_toolkits.mplot3d import Axes3D
 
 class KNNModel(object):
-    def __init__(self,user_id):
+    def __init__(self, user_id):
         self.db = db
         self.genres = None
         self.genre_indices = None
         self.books = None
+        self.user_id = user_id
 
     def generate_n_dimensional_space(self):
         # Fetch all books in the database
@@ -19,8 +20,7 @@ class KNNModel(object):
         # Get all unique genres
         self.genres = set()
         for book in self.books:
-            _, _, _, _, _ = book
-            book_genres = self.db.get_book_genres(book[id])
+            book_genres = self.db.get_book_genres(book['id'])
             for genre in book_genres:
                 self.genres.add(genre)
 
@@ -33,35 +33,35 @@ class KNNModel(object):
 
         # Fill n-dimensional space
         for i, book in enumerate(self.books):
-            _, _, _, _, _ = book
-            book_genres = self.db.get_book_genres(book[id])
-            avg_rating = self.db.get_average_rating(book[id])
+            book_genres = self.db.get_book_genres(book['id'])
+            avg_rating = self.db.get_average_rating(book['id'])
             n_space[i][0] = avg_rating  # Rating as the first dimension
             for genre in book_genres:
                 genre_index = self.genre_indices[genre]
                 n_space[i][genre_index + 1] = 1  # Set genre index to 1
 
         # Visualize the n-dimensional space
+        pca = PCA(n_components=3)
+        n_space_pca = pca.fit_transform(n_space)
         fig = plt.figure()
         ax = fig.add_subplot(111, projection='3d')
-        ax.scatter(n_space[:,0], n_space[:,1], n_space[:,2])  # Example for 3 dimensions
-        ax.set_xlabel('Rating')
-        ax.set_ylabel('Genre1')
-        ax.set_zlabel('Genre2')
+        ax.scatter(n_space_pca[:, 0], n_space_pca[:, 1], n_space_pca[:, 2])
+        ax.set_xlabel('PCA Component 1')
+        ax.set_ylabel('PCA Component 2')
+        ax.set_zlabel('PCA Component 3')
         plt.show()
 
-    def recommendation_algorithm(self, user_id):
+    def recommendation_algorithm(self):
         # Check if genres and genre_indices are initialized
         if self.genres is None or self.genre_indices is None or self.books is None:
             raise ValueError("Genres, genre_indices, or books are not initialized. Run generate_n_dimensional_space first.")
 
         # Using the user_id, fetch their favorite books
-        favorite_books = self.db.get_user_favorites(user_id)
+        favorite_books = self.db.get_user_favorites(self.user_id)
 
         # Create a vector for the user's favorite genres
         user_genre_vector = np.zeros(len(self.genres))
         for book_id in favorite_books:
-            _, _, _, _, _ = book
             book_genres = self.db.get_book_genres(book_id)
             for genre in book_genres:
                 if genre in self.genre_indices:
@@ -73,9 +73,8 @@ class KNNModel(object):
         # Find nearby books with a slight bias towards higher-rated books of the same genre(s)
         recommended_books = []
         for i, book in enumerate(self.books):
-            _, _, _, _, _ = book
-            book_genres = self.db.get_book_genres(book_id)
-            book_rating = self.db.get_rating(book_id)
+            book_genres = self.db.get_book_genres(book['id'])
+            book_rating = self.db.get_rating(book['id'])
             book_genre_vector = np.zeros(len(self.genres))
             for genre in book_genres:
                 if genre in self.genre_indices:
@@ -84,7 +83,7 @@ class KNNModel(object):
             genre_distance = np.linalg.norm(user_genre_vector - book_genre_vector)
             rating_distance = abs(user_avg_rating - book_rating)
             distance = genre_distance + 0.1 * rating_distance  # Bias towards higher-rated books
-            recommended_books.append((book_id, distance))
+            recommended_books.append((book['id'], distance))
 
         # Sort recommended books by distance and return
         recommended_books.sort(key=lambda x: x[1])
